fd34b7caf42a20735c95b02bfafb2263ccd51aa1
Noel Sharpe <noels@radnetwork.co.uk>
2024-12-05 13:33:11 +0000

    hidden Fields not updating.

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,10 +1,9 @@
-import type {ActionFunctionArgs, MetaFunction} from "@remix-run/node";
+import type { ActionFunctionArgs, MetaFunction } from "@remix-run/node";
 import Form from "~/forms/RemixForm";
-import {z} from "zod";
-import {applySchema} from "composable-functions";
-import {createFormAction} from "remix-forms";
-import { redirect, json } from '@remix-run/node'
-
+import { z } from "zod";
+import { applySchema } from "composable-functions";
+import { createFormAction } from "remix-forms";
+import { redirect, json } from "@remix-run/node";
 
 export const addContraSchema = z.object({
   accountId: z.coerce.number(),
@@ -21,45 +20,47 @@ export const saveLedgerSchema = z.object({
   description: z.string().min(3),
   amount: z.coerce.number().multipleOf(0.01).min(0.01),
   ledgerId: z.coerce.number().int(),
-  debitCredit: z.enum(['Debit', 'Credit']),
+  debitCredit: z.enum(["Debit", "Credit"]),
   _action: z.string(),
 });
 
-
-export const action = async ({request}: ActionFunctionArgs) => {
+let buttonAction = "saveLedger";
+export const action = async ({ request }: ActionFunctionArgs) => {
   const formData = await request.clone().formData();
-  const action = formData.get('_action');
-  if (action === 'addContra') {
-    return createFormAction({redirect, json})({
+  const action = formData.get("_action");
+  if (action === "postLedger") {
+    return createFormAction({ redirect, json })({
       request,
       schema: addContraSchema,
-      mutation: addContraMutation,
+      mutation: addPostMutation,
       //environment: {request},
-      successPath: `/`, /* path to redirect on success */
+      successPath: `/` /* path to redirect on success */,
     });
-  } else if (action === 'saveLedger') {
-    return createFormAction({redirect, json})({
+  } else if (action === "saveLedger") {
+    return createFormAction({ redirect, json })({
       request,
       schema: saveLedgerSchema,
       mutation: saveLedgerMutation,
       //environment: {request},
-      successPath: '/', /* path to redirect on success */
+      successPath: "/" /* path to redirect on success */,
     });
   }
-  return {}
-}
+  return {};
+};
 
-const addContraMutation = applySchema(addContraSchema)(async (values) => {
+const addPostMutation = applySchema(addContraSchema)(async (values) => {
+  console.log("postLedger action called");
   console.log(values);
-  return {}
+  buttonAction = "saveLedger";
+  return {};
 });
 
 const saveLedgerMutation = applySchema(saveLedgerSchema)(async (values) => {
+  console.log("saveLedger action called");
   console.log(values);
-  return {}
+  buttonAction = "postLedger";
+  return {};
 });
-
-
 
 export const meta: MetaFunction = () => {
   return [
@@ -69,14 +70,28 @@ export const meta: MetaFunction = () => {
 };
 
 export default function Index() {
+  console.log("Rendering form with action: " + buttonAction);
   return (
     <>
       <Form
         schema={saveLedgerSchema}
-        radio={['debitCredit']}
-        hiddenFields={['accountId', 'ledgerId', '_action']}
-        values={{accountId:2, ledgerId: 1, _action: 'saveLedger'}}
-      />
+        radio={["debitCredit"]}
+        hiddenFields={["accountId", "ledgerId", "_action"]}
+        values={{ accountId: 2, ledgerId: 1, _action: buttonAction }}
+      >
+        {({ Field, Errors, Button }) => (
+          <>
+            <Field name="txDate" />
+            <Field name="description" />
+            <Field name="amount" />
+            <Field name="debitCredit" />
+            <Field name="accountId" />
+            <Field name="ledgerId" />
+            <Field name="_action" />
+            <Button>{buttonAction}</Button>
+          </>
+        )}
+      </Form>
     </>
   );
 }
